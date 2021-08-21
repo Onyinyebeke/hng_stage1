fe40b5a97e39336a28a4f465b8f304744ca7016d
Onyinyebeke <onyinye.chiatula@gmail.com>
2021-08-21 13:24:59 +0100

    First Commit

---
+++ hellogit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hellogit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World, I'm onyi");
+        }
+    }
+}
